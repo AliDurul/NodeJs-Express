a73140dbe8e6186ad36cbf1d3d49f0d27308266a
Ali <alidrl26@gmail.com>
2023-09-12 17:45:37 +0200

    intro express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) => {
+  res.status(200).send('home page')
+})
+
+app.get('/about', (req,res) => {
+  res.status(200).send("this is about page")
+})
+
+app.all('*', (req,res) => {
+    res.status(404).send("Resource not found")
+  })
+
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000');
+})
