65fac7c6c85aa2d132089d2bfb6a04d2cc9e165e
Ali <alidrl26@gmail.com>
2023-09-17 11:05:37 +0200

    manul header

--- Clarus/03_NodeJs/index.js
+++ Clarus/03_NodeJs/index.js
@@ -1,19 +1,37 @@
 "use strict";
 /*-------------------------------------
 
-NODEJS
+//?NODEJS
 
 -------------------------------------*/
 
 const http = require("node:http");
 
-const app = http.createServer((req, res) => {
-  res.end("welcome to nodejs");
-});
+const app = http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.statusCode = 400;
+      res.statusMessage = "not found";
 
-app.listen(8000, () => {
-  console.log('server is running at http://127.0.0.1:8000');
-})
+      res.setHeader("Content-type", "text/html");
+      res.setHeader("another-type", "another/value");
 
+      res.write("* satir 1");
+      res.end("Main Page");
+    } else if (req.url === "/second") {
+      if (req.method == "GET") {
+        res.end("GET");
+      } else {
+        res.end("not supported req");
+      }
+    } else {
+      res.end("Ops ! Page not found");
+    }
+  })
+  .listen(5000, () => {
+    console.log("server is running at http://127.0.0.1:5000");
+  });
+
+/* -------------------------------------*/
 
 /* -------------------------------------*/
