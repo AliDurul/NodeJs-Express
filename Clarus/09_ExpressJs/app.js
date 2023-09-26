bee45027ccefc2487177248023cfb85c7d83fc2a
Ali <alidrl26@gmail.com>
2023-09-26 09:43:44 +0200

    app route

--- Clarus/09_ExpressJs/app.js
+++ Clarus/09_ExpressJs/app.js
@@ -7,9 +7,32 @@ require("dotenv").config();
 const PORT = process.env?.PORT || 8000;
 const HOST = process.env?.HOST || "//localhost";
 
-app.get("/", (req, res) => {
-  res.send("ali durul");
+/* app.get("/", (req, res) => {
+  //   res.send("ali durul");
+  res.send({ mss: "called by get methods" });
 });
+
+app.post("/", (request, response) =>
+  response.send({ message: "called in 'post' method." })
+);
+app.put("/", (request, response) =>
+  response.send({ message: "called in 'put' method." })
+);
+app.delete("/", (request, response) =>
+  response.send({ message: "called in 'delete' method." })
+);
+
+app.all('/',(req,res) => {
+    response.send({ message: "All methods." })
+}) */
+
+//? app.route
+app.route('/')
+.get((req,res) => { res.send('get')})
+.post((req,res) => { res.send('post')})
+.put((req,res) => { res.send('put')})
+.delete((req,res) => { res.send('delete')})
+
 
 app.listen(PORT, () => {
   console.log(`Running on ${HOST}:${PORT}`);
