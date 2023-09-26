72def95c56245a30045e2c96bec7ab0a9a841378
Ali <alidrl26@gmail.com>
2023-09-26 13:52:50 +0200

    query and params

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -27,6 +27,27 @@ app.get("/api/products/:productId", (req, res) => {
   return res.json(singleProduct);
 });
 
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send('no product found')
+    return res.status(200).json({ success: true, data: [] });
+  }
+
+  return res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log("this is server runining");
 });
