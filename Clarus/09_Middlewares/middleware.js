293df27f5dde83bc6063f743db22899d960fd595
Ali <alidrl26@gmail.com>
2023-09-28 20:52:03 +0200

    pass parameters with next

--- Clarus/09_Middlewares/middleware.js
+++ Clarus/09_Middlewares/middleware.js
@@ -6,26 +6,33 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
+//--------------------------------
 
-app.get('/', (req,res) => {
-  
-})
+app.get("/", (req, res, next) => {
+    
+  req.inReq = "custom data";
+  res.InResponse = "custom data response";
 
+  next();
 
+  res.send({
+    msg: "middleware...",
+  });
+});
 
+app.get("/", (req, res, next) => {
+    
 
+  res.send({
+    msg: "ortadaki",
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.send({
+    customData: [req.inReq, res.InResponse],
+    msg: "welcome",
+  });
+});
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
