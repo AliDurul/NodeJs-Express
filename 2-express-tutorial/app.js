1f859caac037f59db8773cf3a4ec5a13e5c654df
Ali <alidrl26@gmail.com>
2023-09-28 20:05:32 +0200

    logger

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,51 +1,22 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const logger = require("./logger");
+
+
+app.use(logger)
+
 
 app.get("/", (req, res) => {
-  res.send(`
-  <h1> Home Page</h1>
-  <a href="/api/products">products</a> <br>
-  <a href="/api/products/1">product </a>
-  `);
+  res.send("Home");
 });
-
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  res.send("Products");
 });
-
-app.get("/api/products/:productId", (req, res) => {
-  const { productId } = req.params;
-  const singleProduct = products.find((product) => product.id === +productId);
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exit");
-  }
-  return res.json(singleProduct);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no product found')
-    return res.status(200).json({ success: true, data: [] });
-  }
-
-  return res.status(200).json(sortedProducts);
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(5000, () => {
