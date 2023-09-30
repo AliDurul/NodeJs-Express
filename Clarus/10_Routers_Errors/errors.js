a0c8aae9765f1edd4579947b0b83c20db113e214
Ali <alidrl26@gmail.com>
2023-09-30 12:52:23 +0200

    express-async-handler

--- Clarus/10_Routers_Errors/errors.js
+++ Clarus/10_Routers_Errors/errors.js
@@ -29,7 +29,7 @@ app.get("/user/:id", (req, res) => {
   }
 });
 
-/* ------------------------------------------------------------ */
+/* ------------------------------------------------------------ *
 
 app.get("/user/:id", (req, res) => {
   const id = req.params.id ?? 0;
@@ -45,10 +45,57 @@ app.get("/user/:id", (req, res) => {
   }
 });
 
+/* ===================================================== *
+
+app.get("/user/:id", (req, res, next) => {
+  const id = req.params.id ?? 0;
+
+  try {
+    if (isNaN(id)) {
+      res.statusCode = 400;
+      throw new Error("ID is Not A Number", { cause: "params.id=" + id });
+    } else {
+      res.send({
+        error: false,
+        id: id,
+      });
+    }
+  } catch (err) {
+    console.log("try catch runned");
+    next(err); // goes error handler
+  }
+});
+
+/* ===================================================== *
+
+//! ASYNC
+
+const asyncFuntion = async () => {
+    throw new Error("CXreated error in async0funcr");
+}
+
+app.get('/async',async (req,res,next) => {
+  await asyncFuntion().catch(next)  //goes error handler
+})
+
+/* ===================================================== */
+//! express-async-handler
+const asyncHandler = require('express-async-handler')
+
+app.get('/async', asyncHandler(async (req, res, next) => {
+    res.statusCode = 400
+    throw new Error('Created error in async-func')
+}))
+
+
+/* ===================================================== */
+
+
+//!error handler
 const errorHandeler = (err, req, res, next) => {
   console.log("errorHandler runned");
 
-  const statusCode = res.statusCode ?? 500;
+  const statusCode = res.errorStatusCode ?? 500;
 
   res.status(statusCode).send({
     error: true,
@@ -56,9 +103,6 @@ const errorHandeler = (err, req, res, next) => {
     cause: err.cause,
   });
 };
-
 app.use(errorHandeler);
-
-/* ===================================================== */
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
