1e370dd99954764a5b79de6b3d09abb661b317b7
Ali <alidrl26@gmail.com>
2023-10-02 07:18:50 +0200

    Models & Sequelize intro

---
+++ Clarus/11_Models_Sequeilze/server.js
@@ -0,0 +1,45 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// Accept json data:
+app.use(express.json())
+
+app.all('/', (req, res) => {
+    res.send('WELCOME TO TODO API')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const errorHandler = (err, req, res, next) => {
+    const errorStatusCode = res.errorStatusCode ?? 500
+    console.log('errorHandler runned.')
+    res.status(errorStatusCode).send({
+        error: true, // special data
+        message: err.message, // error string message
+        cause: err.cause, // error option cause
+        // stack: err.stack, // error details
+    })
+}
+app.use(errorHandler)
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
