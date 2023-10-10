b1f0e8873e666d67d6bb25f8a9f3c8e6a36f913b
Ali <alidrl26@gmail.com>
2023-10-10 21:15:35 +0200

    model and controller added

--- Clarus/16-PersonnelAPI/src/models/department.model.js
+++ Clarus/16-PersonnelAPI/src/models/department.model.js
@@ -1,6 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const DepartmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { collection: "departments", timestamps: true }
+);
+
+module.exports = mongoose.model('Department', DepartmentSchema)
